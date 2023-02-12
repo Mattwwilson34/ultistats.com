ab81ccb134200c7baa9bfd1b0962e25db1ed245e
Matt Wilson <mattwwilson34@gmail.com>
2023-02-12 12:09:04 -0500

    Update: login and root route return values and types

---
+++ frontend/src/routes/login.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import LoginButton from '../features/login'
+const Login = (): JSX.Element => {
+  return <LoginButton />
+}
+
+export default Login
---
+++ frontend/src/routes/root.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import App from '../App'
+
+const Root = (): JSX.Element => {
+  return <App />
+}
+
+export default Root
