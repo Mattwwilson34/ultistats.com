9376db9c1c40b66eaf7de19e636513cdea363bac
Matt Wilson <mattwwilson34@gmail.com>
2023-02-12 12:05:51 -0500

    Add: '/' route loader func to check user login status and redirect if needed

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { createBrowserRouter, redirect, RouterProvider } from 'react-router-dom'
+import Root from './routes/root'
+import Login from './routes/login'
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    loader: () => {
+      const user = localStorage.getItem('firebaseUser')
+      if (user != null) {
+        return <Root />
+      } else {
+        return redirect('/login')
+      }
+    },
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
