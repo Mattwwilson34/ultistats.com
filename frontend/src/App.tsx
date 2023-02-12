d809426fc6c7f251dfd38afa29fcebdf4a9fdc45
Matt Wilson <mattwwilson34@gmail.com>
2023-02-11 19:08:52 -0500

    Add: prettier configs and tested prettier format on save in neovim

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,7 @@
 import React from 'react'
 
-function App (): React.ReactElement {
-  return (
-    <div className="App">
-      Firbase
-    </div>
-  )
+function App(): React.ReactElement {
+  return <div className="App">Firbase</div>
 }
 
 export default App
