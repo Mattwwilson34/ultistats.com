d93529766677c53e700d722d66d08631789ae1a3
Matt Wilson <mattwwilson34@gmail.com>
2023-02-12 12:19:40 -0500

    Refactor: copy

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { Button } from '@mui/material'
 
 function App(): React.ReactElement {
-  return <div className="App">Firbase</div>
+  return (
+    <div className="App">
+      <Button variant="text">Welcom to the home page!!</Button>
+    </div>
+  )
 }
 
 export default App
