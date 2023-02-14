1de7696b664d56a19ab47bc132054ffcad20b42a
Matt Wilson <mattwwilson34@gmail.com>
2023-02-14 13:51:23 -0500

    Refactor: component size reduction, add check if user in db if not add to db

--- frontend/src/features/login/components/LoginButton.tsx
+++ frontend/src/features/login/components/LoginButton.tsx
@@ -1,32 +1,51 @@
-import React, { useEffect } from 'react'
-import { Button } from '@mui/material'
-import { useNavigate } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Button, CircularProgress } from '@mui/material'
 import { getRedirectResult, signInWithRedirect } from 'firebase/auth'
 import { auth, provider } from '../../../../../firebase/firebase'
 import Logo from '../../../assets/logo-color.svg'
+import checkUserExists from '../../../utils/check-user-exists'
+import saveUserDataToDB from '../../../utils/save-user-to-db'
 
 const LoginButton = (): JSX.Element => {
-  const navigate = useNavigate()
+  const [redirected, setRedirected] = useState(false)
 
   useEffect(() => {
     const getUser = async (): Promise<void> => {
       try {
         const response = await getRedirectResult(auth)
-        if (response !== null) {
-          const { providerData } = response.user
-          localStorage.setItem('firebaseUser', JSON.stringify(providerData[0]))
-          navigate('/')
+        if (response == null) {
+          throw Error('Response undefined')
         }
+
+        const userData = response.user?.providerData[0]
+        if (userData === null || userData === undefined) {
+          throw Error('User data undefined')
+        }
+
+        const userExistsInDB: boolean = await checkUserExists(userData)
+        if (userExistsInDB) {
+          localStorage.removeItem('redirectedFromGoogleAuth')
+          window.location.href = '/'
+        }
+
+        await saveUserDataToDB(userData)
+        localStorage.removeItem('redirectedFromGoogleAuth')
+        window.location.href = '/'
       } catch (error) {
-        console.log(error)
+        console.error(error)
       }
     }
-    void getUser()
+    const redirected = localStorage.getItem('redirectedFromGoogleAuth')
+    if (redirected != null) {
+      setRedirected((prevRedirected) => !prevRedirected)
+      void getUser()
+    }
   }, [])
 
   const handleLogin = (): void => {
     void (async () => {
       try {
+        localStorage.setItem('redirectedFromGoogleAuth', 'true')
         await signInWithRedirect(auth, provider)
       } catch (error) {
         console.log(error)
@@ -36,10 +55,18 @@ const LoginButton = (): JSX.Element => {
 
   return (
     <>
-      <img src={Logo} alt="UltiStats logo" />
-      <Button variant="contained" size="large" onClick={handleLogin}>
-        Login
-      </Button>
+      {!redirected ? (
+        <>
+          <img src={Logo} alt="UltiStats logo" />
+          <Button variant="contained" size="large" onClick={handleLogin}>
+            Login
+          </Button>
+        </>
+      ) : (
+        <div>
+          <CircularProgress />
+        </div>
+      )}
     </>
   )
 }
