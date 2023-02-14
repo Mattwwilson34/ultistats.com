ba8177b2bff1ab81d1d5c2f71e6d4c06c6f8672a
Matt Wilson <mattwwilson34@gmail.com>
2023-02-13 19:25:09 -0500

    Add: user name from localStorage to welcome message

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Button } from '@mui/material'
+import userContext from './utils/user-context'
 
 function App(): React.ReactElement {
+  const user = useContext(userContext) ?? {displayName: 'User not found in DB'}
   return (
     <div className="App">
-      <Button variant="text">Welcom to the home page!!</Button>
+      <Button variant="text">
+        {`Welcome ${user?.displayName}`}
+      </Button>
     </div>
   )
 }
