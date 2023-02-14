08a9efac9a831a7fae57a489317fb3db82dc0354
Matt Wilson <mattwwilson34@gmail.com>
2023-02-14 13:08:13 -0500

    Add: AuthContext to check if user logged in and provide user context or redirect

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,22 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, redirect, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Root from './routes/root'
 import Login from './routes/login'
 import './index.css'
+import { AuthProvider } from './Auth/AuthContext'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
-    loader: () => {
-      const user = localStorage.getItem('firebaseUser')
-      if (user != null) {
-        return <Root />
-      } else {
-        return redirect('/login')
-      }
-    },
   },
   {
     path: '/login',
@@ -25,7 +18,9 @@ const router = createBrowserRouter([
 ])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <AuthProvider>
     <RouterProvider router={router} />
-  </React.StrictMode>
+  </AuthProvider>
+  // </React.StrictMode>
 )
--- frontend/src/routes/root.tsx
+++ frontend/src/routes/root.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import App from '../App'
+import { AuthContext } from '../Auth/AuthContext'
 
 const Root = (): JSX.Element => {
+  const { currentUser } = useContext(AuthContext)
+  if (currentUser == null) {
+    window.location.href = '/login'
+  }
   return <App />
 }
 
