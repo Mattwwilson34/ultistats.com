44fb1a5a2cc62efce148e0f464fc115d8f2ba106
Matt Wilson <mattwwilson34@gmail.com>
2023-02-17 14:08:00 -0500

    refactor(LoginButton): move logo to Login route component

--- frontend/src/routes/login.tsx
+++ frontend/src/routes/login.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import LoginButton from '../features/login'
+import Logo from '../assets/logo-color.svg'
 const Login = (): JSX.Element => {
-  return <LoginButton />
+  return (
+    <div className="Login" data-testid="Login">
+      <img src={Logo} alt="UltiStats logo" />
+      <LoginButton />
+    </div>
+  )
 }
 
 export default Login
