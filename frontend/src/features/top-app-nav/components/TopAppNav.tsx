7e232f2b1f812ef3f7057a2534127d0546f14774
Matt Wilson <mattwwilson34@gmail.com>
2023-02-17 19:58:06 -0500

    feat(TopAppNav): add TopAppNav component

---
+++ frontend/src/features/top-app-nav/components/TopAppNav.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react'
+import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+import IconButton from '@mui/material/IconButton'
+import logoutUser from '../../../utils/logout-user'
+
+export default function TopAppNav(): React.ReactElement {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar disableGutters>
+          <IconButton color="inherit" aria-label="menu"></IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            UltiStats
+          </Typography>
+          <Button color="inherit" onClick={logoutUser}>
+            Logout
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  )
+}
