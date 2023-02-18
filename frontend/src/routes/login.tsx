96f2cc15918a04c4b1e3d9efc37c37d0381ab615
Matt Wilson <mattwwilson34@gmail.com>
2023-02-18 18:25:01 -0500

    styles(login): remove logo from login component

--- frontend/src/routes/login.tsx
+++ frontend/src/routes/login.tsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import LoginButton from '../features/login'
-import Logo from '../assets/logo-color.svg'
 const Login = (): JSX.Element => {
   return (
     <div className="Login" data-testid="Login">
-      <img src={Logo} alt="UltiStats logo" />
       <LoginButton />
     </div>
   )
