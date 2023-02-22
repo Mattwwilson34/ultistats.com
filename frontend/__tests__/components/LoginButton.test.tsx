5523fddb4605ea60934691f800f8196bd00c7c0f
Matt Wilson <mattwwilson34@gmail.com>
2023-02-22 11:43:14 -0500

    test(LoginButton): eslint unused vars error so removed rule for file

--- frontend/__tests__/components/LoginButton.test.tsx
+++ frontend/__tests__/components/LoginButton.test.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable no-unused-vars */
+
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { render, screen, waitFor } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
