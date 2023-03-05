af871eea84d92e40ab51c3e966106f673c7a44e1
Matt Wilson <mattwwilson34@gmail.com>
2023-03-05 12:00:17 -0500

    chore(utils): formatting

--- frontend/src/utils/get-teams.ts
+++ frontend/src/utils/get-teams.ts
@@ -6,15 +6,19 @@ type Teams = Team[]
 
 const getTeams = async (userUid: string): Promise<Teams> => {
   const teams: Teams = []
-  const teamsRef = collection(db, 'teams')
-  const q = query(teamsRef, where('userUid', '==', userUid))
-  const querySnapshot = await getDocs(q)
-  querySnapshot.forEach((doc) => {
-    // Fix: teamName is undefined in this scope. Moving on with dev for now.
-    const team = { ...doc.data() } as Team // eslint-disable-line @typescript-eslint/consistent-type-assertions
+  try {
+    const teamsRef = collection(db, 'teams')
+    const q = query(teamsRef, where('userUid', '==', userUid))
+    const querySnapshot = await getDocs(q)
+    querySnapshot.forEach((doc) => {
+      // Fix: teamName is undefined in this scope. Moving on with dev for now.
+      const team = { ...doc.data() } as Team // eslint-disable-line @typescript-eslint/consistent-type-assertions
 
-    teams.push(team)
-  })
+      teams.push(team)
+    })
+  } catch (error) {
+    console.log(error)
+  }
   return teams
 }
 
